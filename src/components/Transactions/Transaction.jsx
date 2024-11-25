98839e16ca9a6dc42dc1ca503aef1cff3de44d33
Jan Świątek <swiatek-jan@wp.pl>
2024-11-25 18:29:45 +0100

    TransactionDone

--- src/components/Transactions/Transaction.jsx
+++ src/components/Transactions/Transaction.jsx
@@ -1,26 +1,37 @@
+import PropTypes from 'prop-types';
+import css from './Transaction.module.css';
 
-
-
-
-<table class="transaction-history">
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
+const TransactionHistory = ({ items }) => {
+  const elements = items.map(({ id, type, amount, currency }) => (
+    <tr key={id}>
+      <td>{type}</td>
+      <td>{amount}</td>
+      <td>{currency}</td>
     </tr>
-  </thead>
+  ));
 
-  <tbody>
-    <tr>
-      <td>Invoice</td>
-      <td>125</td>
-      <td>USD</td>
-    </tr>
-    <tr>
-      <td>Withdrawal</td>
-      <td>85</td>
-      <td>USD</td>
-    </tr>
-  </tbody>
-</table>
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>{elements}</tbody>
+    </table>
+  )
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.number.isRequired,
+      currency: PropTypes.string.isRequired
+    }).isRequired
+  ),
+};
